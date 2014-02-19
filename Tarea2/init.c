e6c872df5aa84cb0856cdb67b58e50329b1e1834
Fabian92mx <fabian92_mex@hotmail.com>
2014-02-19 17:36:37 -0600

    agregado lo de carlitos

--- Tarea2/init.c
+++ Tarea2/init.c
@@ -1,2 +1,28 @@
 //Creates 6 processes that will be transformed into getty processes via Xterm
 //If a process dies, init creates another one.
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <sys/wait.h> 
+int main()
+	{
+	int pid; 
+	int status;
+	int procesos = 6; 
+	int i; 
+	for(i=0;i<procesos; i++) 
+	{ 
+		pid = fork(); 
+		if(pid ==0) 
+		{ 
+		execlp("xterm","xterm","-e","./getty",0); 
+		} 
+	}
+	while(1) 
+	{ 
+	waitpid(-1,&status,0); 
+	pid=fork();
+	if(pid==0) 
+	{ execlp("xterm","xterm","-e","./getty",0); 
+}
+}
+}
