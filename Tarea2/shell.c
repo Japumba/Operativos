7cb99c602a2c3e1da15c1f516972ce14fa90d51b
Japumba <jdelamorav@hotmail.com>
2014-02-19 17:35:11 -0600

    Shell dummy.

--- Tarea2/shell.c
+++ Tarea2/shell.c
@@ -1,3 +1,12 @@
-//Command interpreter.
-//Each shell has ambient variables. A special variable "Path" is used to run programs
-//not available in the current directory.
+/*
+Command interpreter.
+Each shell has ambient variables. A special variable 
+"Path" is used to run programs not available in the
+current directory.
+*/
+
+int main(int argc, char *argv[])
+{
+	char *input;
+	return 0;
+}
